6ee74861820694ede2bcd6a36a0e284ded5cadea
Igor Medvedenko <noveciti0@gmail.com>
2024-11-03 21:20:03 +0200

    task1

--- js/task-1.js
+++ js/task-1.js
@@ -1,1 +1,12 @@
-function user = () =>
+const categoriesList = document.getElementById("categories");
+const categoriesItems = categoriesList.querySelectorAll(".item");
+
+console.log("Number of categories:", categoriesItems.length);
+
+categoriesItems.forEach((item) => {
+  const categoryTitle = item.querySelector("h2").textContent;
+  const categoryElements = item.querySelectorAll("li");
+
+  console.log(`Category: ${categoryTitle}`);
+  console.log(`Elements: ${categoryElements.length}`);
+});
